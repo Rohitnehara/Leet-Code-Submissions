ce256f34ed3db1c553040754c7554e5faddcd802
Rohit Nehara <91176152+Rohitnehara@users.noreply.github.com>
2022-11-03 18:35:58 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        int []indegree = new int [V];
+        for(int i =0;i<V;i++){
+            for(int it :adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue <Integer> q = new LinkedList<>();
+         for(int i =0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int count=0;
+        int []ans  = new int[V];
+        while(q.size()>0){
+            int curNode = q.remove();
+            ans[count] =curNode;
+            count++;
+            for(int it :adj.get(curNode)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                    
+                }
+            }
+        }
+        if(count !=V){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
