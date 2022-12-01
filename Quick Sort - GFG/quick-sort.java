5025ec7a3d7bbbb987a457ed29742a24b7144601
Rohit Nehara <91176152+Rohitnehara@users.noreply.github.com>
2022-12-01 12:16:21 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.util.*;
+class Sorting
+{
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    
+    // Driver program
+    public static void main(String args[])
+    {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+			
+			new Solution().quickSort(arr,0,n-1);
+			printArray(arr);
+		    T--;
+		}
+} }
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low<high){
+        int pivot = partition(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+        }
+    }
+    static void swap(int arr[],int j ,int i){
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]= temp;
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        int pi = low;
+        int i =low;
+        int j =high;
+        while(i<j){
+            while(i<high && arr[i]<=arr[pi]){
+            i++;
+        }
+            while(j>low && arr[j]>arr[pi]){
+                j--;
+            } 
+            if(i<j){
+                swap(arr,i,j);
+            }
+        }  
+        swap(arr,j,low);
+        return j;
+        
+    } 
+}
