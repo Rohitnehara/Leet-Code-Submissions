5e1ac118cee08f29b23e57308d897e5aab2eae39
Rohit Nehara <91176152+Rohitnehara@users.noreply.github.com>
2023-06-24 16:03:27 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+
+
+class Flatttening_A_LinkedList
+{	
+    Node head;
+	
+	void printList(Node node)
+    {
+        //Node temp = head;
+        while (node != null)
+        {
+            System.out.print(node.data + " ");
+            node =node.bottom;
+        }
+        System.out.println();
+    }
+	public  static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		Flatttening_A_LinkedList list = new Flatttening_A_LinkedList();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			int arr[] = new int[N];
+			for(int i=0;i<N;i++)
+				arr[i] = sc.nextInt();
+			
+			Node temp = null;
+			Node tempB = null;
+			Node pre = null;
+			Node preB = null;	
+			int flag=1;
+			int flag1=1;
+			for(int i=0; i<N;i++)
+			{
+				int m = arr[i];
+				m--;
+				int a1 = sc.nextInt();
+				temp = new Node(a1);
+				if(flag == 1)
+				{
+					list.head = temp;
+					pre = temp;
+					flag = 0;
+					flag1 = 1;
+				}
+				else
+				{
+					pre.next = temp;
+					pre = temp;
+					flag1 = 1;
+				}
+				
+				for(int j=0;j<m;j++)
+				{
+					int a = sc.nextInt();
+					tempB = new Node(a);
+					if(flag1 == 1)
+					{
+						temp.bottom = tempB;
+						preB = tempB;
+						flag1 = 0;
+					}
+					else
+					{
+						preB.bottom = tempB;
+						preB = tempB;
+					}
+				}
+				
+			}
+			//list.printList();
+			GfG g = new GfG();
+			Node root = g.flatten(list.head);
+			list.printList(root);
+		
+		t--;
+		}
+	}	
+}
+// } Driver Code Ends
+
+
+/*Node class  used in the program
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+*/
+/*  Function which returns the  root of 
+    the flattened linked list. */
+class GfG
+{
+    Node flatten(Node root)
+    {
+	// Your code here
+	Node dummyHead =new Node(-1);
+	Node node =root;
+	List<Node>li=new ArrayList<>();
+	while(node!=null){
+	    li.add(node);
+	    node=node.next;
+	}
+	Node temp=dummyHead;
+	while(li.size()>0){
+	    int minVal=Integer.MAX_VALUE;
+	    int minIndex=-1;
+	   // System.out.println(li);
+	    for(int i=0;i<li.size();i++){
+	        int val=li.get(i).data;
+	        if(minVal>val){
+	            minVal=val;
+	            minIndex=i;
+	        }
+	       
+	    }
+        temp.bottom=new Node(minVal);
+        temp=temp.bottom;
+        Node n  =li.get(minIndex);
+        if(n.bottom==null){
+            li.remove(minIndex);
+        }else{
+            li.set(minIndex,n.bottom);
+        }
+    
+	} 
+ 
+	return dummyHead.bottom;
+    }
+}
