c93d579aa8a860f58e7fc1fb04f7effc2f67bfa2
Rohit Nehara <91176152+Rohitnehara@users.noreply.github.com>
2023-06-26 18:43:26 +0530

    Added solution - LeetHub

---
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.java
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+public class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution ob = new Solution();
+
+                ArrayList<Integer> vec = ob.topView(root);
+                for(int x : vec)
+                    System.out.print(x + " ");
+                System.out.println();
+        	
+                t--;   
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution
+{
+    static class Pair {
+        Node node;
+        int level;
+        int hd;
+        Pair(Node node,int level,int hd){
+            this.node=node;
+            this.level =level;
+            this.hd = hd;
+        }
+    }
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        // add your code
+       Queue<Pair> q =new ArrayDeque<>();
+       q.add(new Pair(root,0,0));
+       HashMap<Integer,Integer>hm=new HashMap<>();
+       while(q.size()>0){
+           Pair p =q.remove();
+           if(!hm.containsKey(p.hd)){
+               hm.put(p.hd,p.node.data);
+           }
+           if(p.node.left!=null){
+               q.add(new Pair(p.node.left,p.level+1,p.hd-1));
+           }
+           if(p.node.right!=null){
+               q.add(new Pair(p.node.right,p.level+1,p.hd+1));
+           }
+       }
+       ArrayList<Integer>li=new ArrayList<>();
+       li.addAll(hm.keySet());
+       Collections.sort(li);
+       ArrayList<Integer>ans =new ArrayList<>();
+       for(int i=0;i<li.size();i++){
+            ans.add(hm.get(li.get(i)));
+       }
+       return ans;
+    }
+}
